142ef82f9dcb64f4502fa9442ef5229ef515147a
Xheni Rapushaj <xhenicoding@gmail.com>
2024-11-08 07:58:14 +0100

    feat: add product table (#5)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from './app/store';
 import Login from './components/Login';
+import ProductTable from './components/ProductTable';
 
 function App() {
   const isAuthenticated = useSelector((state: RootState) => state.auth.token);
 
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <Login />
+      {isAuthenticated ? <ProductTable /> : <Login />}
     </div>
   );
 }
